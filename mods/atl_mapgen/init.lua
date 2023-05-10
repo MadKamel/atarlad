703eb8301070b2266a612fda28eb82437f998942
Eric K Rustrum <rustrume184@svusd.net>
2023-05-10 15:07:58 -0700

    mapgen spawns a single block.

--- mods/atl_mapgen/init.lua
+++ mods/atl_mapgen/init.lua
@@ -1,1 +1,9 @@
 minetest.set_mapgen_setting("mg_name", "singlenode", true)
+
+minetest.register_on_newplayer(function(player)
+	player:set_pos({x = 0, y = 2, z = 0})
+end)
+
+minetest.register_on_generated(function(minp, maxp, seed)
+	minetest.set_node({x = 0, y = 0, z = 0}, {name = "atl_rock:granite"})
+end)
