9c7772c90ed8d6df7409e27efa90be2918a584ed
Eric Rustrum <rustrumek@gmail.com>
2023-05-10 12:18:37 -0700

    Changed groups for nodes.

--- mods/atl_registrations/init.lua
+++ mods/atl_registrations/init.lua
@@ -4,7 +4,7 @@ atl.register_rock = function(nodename, nodedata)
 	minetest.register_node(":atl_rock:"..nodename, {
 		description = "Clean " .. nodedata.description,
 		tiles = {"atl_rock."..nodename.."_node.png"},
-		groups = {chisel = nodedata.hardness, rock = 1},
+		groups = {rock = nodedata.hardness},
 		drop = "",
 		after_dig_node = function(pos, oldnode, oldmetadata, digger)
 			minetest.set_node(pos, {name = ":atl_rock:"..nodename.."_broken"})
@@ -13,7 +13,7 @@ atl.register_rock = function(nodename, nodedata)
 	minetest.register_node(":atl_rock:"..nodename.."_broken", {
 		description = "Chipped " .. nodedata.description,
 		tiles = {"atl_rock."..nodename.."_node_1.png"},
-		groups = {chisel = nodedata.hardness, broken_rock = 1},
+		groups = {rock = nodedata.hardness},
 		drop = "",
 		after_dig_node = function(pos, oldnode, oldmetadata, digger)
 			minetest.set_node(pos, {name = ":atl_rock:"..nodename.."_gravel"})
@@ -22,7 +22,7 @@ atl.register_rock = function(nodename, nodedata)
 	minetest.register_node(":atl_rock:"..nodename.."_gravel", {
 		description = "Gravel of " .. nodedata.description,
 		tiles = {"atl_rock."..nodename.."_node_gravel.png"},
-		groups = {shovel = 2, gravel = 1},
+		groups = {gravel = 1},
 		drop = "",
 		after_dig_node = function(pos, oldnode, oldmetadata, digger)
 			minetest.set_node(pos, {name = ":atl_rock:"..nodename.."_gravel"})
