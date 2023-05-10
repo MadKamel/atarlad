fcb49ab39416a53fd7471c3678b8c2dd74bd1a84
Eric Rustrum <rustrumek@gmail.com>
2023-05-10 16:02:24 -0700

    Changed daylight cycle

--- mods/atl_mapgen/init.lua
+++ mods/atl_mapgen/init.lua
@@ -7,3 +7,19 @@ end)
 minetest.register_on_generated(function(minp, maxp, seed)
 	minetest.set_node({x = 0, y = 0, z = 0}, {name = "atl_rock:granite"})
 end)
+
+minetest.register_on_joinplayer(function(player)
+	player:set_sun({
+		visible = true
+	})
+
+	player:set_moon({
+		visible = false
+	})
+
+	player:set_stars({
+		visible = false
+	})
+
+	player:override_day_night_ratio(1)
+end)
