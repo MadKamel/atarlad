774c8c2b7f092aed6418dd1f2e7e78a9b11c20e4
Eric Rustrum <rustrumek@gmail.com>
2023-05-10 12:46:13 -0700

    Fixes

--- mods/atl_registrations/init.lua
+++ mods/atl_registrations/init.lua
@@ -13,7 +13,7 @@ atl.register_rock = function(nodename, nodedata)
 	minetest.register_node(":atl_rock:"..nodename.."_broken", {
 		description = "Chipped " .. nodedata.description,
 		tiles = {"atl_rock."..nodename.."_node_1.png"},
-		groups = {rock = nodedata.hardness},
+		groups = {rock = 1},
 		drop = "",
 		after_dig_node = function(pos, oldnode, oldmetadata, digger)
 			minetest.set_node(pos, {name = ":atl_rock:"..nodename.."_gravel"})
@@ -29,39 +29,39 @@ atl.register_rock = function(nodename, nodedata)
 		end,
 	})
 
-	atl.register_wood = function(nodename, nodedata)
-		minetest.register_node(":atl_flora:"..nodename.."_plank", {
-			description = nodedata.description.." Planks",
-			tiles = {"atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank_cap.png", "atl_flora."..nodename.."_plank_cap.png", "atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank.png"},
-			groups = {wood = nodedata.hardness},
-			paramtype2 = "facedir"
-		})
-		minetest.register_node(":atl_flora:"..nodename, {
-			description = nodedata.description.." Trunk",
-			tiles = {"atl_flora."..nodename..".png"},
-			groups = {wood = nodedata.hardness},
-			drop = "",
-			after_dig_node = function(pos, oldnode, oldmetadata, digger)
-				minetest.set_node(pos, {name = ":atl_flora:"..nodename.."_stripped"})
-			end,
-		})
-		minetest.register_node(":atl_flora:"..nodename.."_stripped", {
-			description = "Stripped "..nodedata.description.." Trunk",
-			tiles = {"atl_flora."..nodename.."_stripped.png"},
-			groups = {wood = nodedata.hardness},
-		})
-		minetest.register_node(":atl_flora:"..nodename.."_cross", {
-			description = nodedata.description.." Trunk",
-			tiles = {"atl_flora."..nodename.."_cross.png"},
-			groups = {wood = nodedata.hardness},
-			drop = "",
-			after_dig_node = function(pos, oldnode, oldmetadata, digger)
-				minetest.set_node(pos, {name = ":atl_flora:"..nodename.."_cross_stripped"})
-			end,
-		})
-		minetest.register_node(":atl_flora:"..nodename.."_cross_stripped", {
-			description = "Stripped "..nodedata.description.." Trunk",
-			tiles = {"atl_flora."..nodename.."_cross_stripped.png"},
-			groups = {wood = nodedata.hardness},
-			drop = "atl_flora:"..nodename.."_stripped",
-		})
+atl.register_wood = function(nodename, nodedata)
+	minetest.register_node(":atl_flora:"..nodename.."_plank", {
+		description = nodedata.description.." Planks",
+		tiles = {"atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank_cap.png", "atl_flora."..nodename.."_plank_cap.png", "atl_flora."..nodename.."_plank.png", "atl_flora."..nodename.."_plank.png"},
+		groups = {wood = nodedata.hardness},
+		paramtype2 = "facedir"
+	})
+	minetest.register_node(":atl_flora:"..nodename, {
+		description = nodedata.description.." Trunk",
+		tiles = {"atl_flora."..nodename..".png"},
+		groups = {wood = nodedata.hardness},
+		drop = "",
+		after_dig_node = function(pos, oldnode, oldmetadata, digger)
+			minetest.set_node(pos, {name = ":atl_flora:"..nodename.."_stripped"})
+		end,
+	})
+	minetest.register_node(":atl_flora:"..nodename.."_stripped", {
+		description = "Stripped "..nodedata.description.." Trunk",
+		tiles = {"atl_flora."..nodename.."_stripped.png"},
+		groups = {wood = nodedata.hardness},
+	})
+	minetest.register_node(":atl_flora:"..nodename.."_cross", {
+		description = nodedata.description.." Trunk",
+		tiles = {"atl_flora."..nodename.."_cross.png"},
+		groups = {wood = nodedata.hardness},
+		drop = "",
+		after_dig_node = function(pos, oldnode, oldmetadata, digger)
+			minetest.set_node(pos, {name = ":atl_flora:"..nodename.."_cross_stripped"})
+		end,
+	})
+	minetest.register_node(":atl_flora:"..nodename.."_cross_stripped", {
+		description = "Stripped "..nodedata.description.." Trunk",
+		tiles = {"atl_flora."..nodename.."_cross_stripped.png"},
+		groups = {wood = nodedata.hardness},
+		drop = "atl_flora:"..nodename.."_stripped",
+	})
